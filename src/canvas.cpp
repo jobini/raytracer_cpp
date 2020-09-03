6c8b3a4aee96a7967fff712b5abb44dee8a9f409
jobini <jobsism10@gmail.com>
2020-09-03 16:50:30 +0530

    fixed bugs in Canvas::to_ppm method, unit tests for PPM pixel data construction

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <cmath>
 
 using std::string;
 
@@ -10,31 +11,48 @@ std::vector<Color>& Canvas::operator[](const size_t i){
     return this->pixels[i];
 }
 
+int clamp(float val){
+    if (val > 255)
+        return 255;
+    if (val < 0)
+        return 0;
+    return int(val);
+}
+
 string Canvas::to_ppm(string filename){
     string ppm = "P3\n" + std::to_string(this->width) + " " + std::to_string(this->height) + "\n255\n";
     string line = "";
-    int line_counter = 0;
+    size_t future_line_size = 0;
     for (size_t i = 0; i < this->height; ++i){
         line = "";
+
         for (size_t j = 0; j < this->width; ++j){
             for (size_t k = 0; k < 3; ++k){
-                line += std::to_string(std::min(int((*this)[i][j][k] * 255), 255));
-                ++line_counter;
-                if (line_counter % 17 == 0)
+                if (line.size() == 0){
+                    future_line_size = line.size() + (std::to_string(clamp(std::ceil((*this)[i][j][k] * 255)))).size();
+                }
+                else
+                    future_line_size = line.size() + (" " + std::to_string(clamp(std::ceil((*this)[i][j][k] * 255)))).size();
+                
+                if (future_line_size <= 70){
+                    if (line.size() == 0)
+                        line += std::to_string(clamp(std::ceil((*this)[i][j][k] * 255)));
+                    else
+                        line += " " + std::to_string(clamp(std::ceil((*this)[i][j][k] * 255)));
+                }
+                else
                 {
                     line += "\n";
                     ppm += line;
-                    line = "";
+                    line = std::to_string(clamp(std::ceil((*this)[i][j][k] * 255)));
+                    future_line_size = 0;
                 }
-                else
-                    line += " "; 
             }
         }
         line += "\n";
         ppm += line;
-        line_counter = 0;
+        future_line_size = 0;
     }
-    ppm += "\n";
     std::ofstream out(OUTPUT_DIR + filename);
     out << ppm;
     out.close();
--- test/canvas_tests.cpp
+++ test/canvas_tests.cpp
@@ -8,7 +8,7 @@
 using std::string;
 
 TEST_CASE("Creating a canvas"){
-    GIVEN ("A canvas c with width 10 and height 20 pixels"){
+    GIVEN("A canvas c with width 10 and height 20 pixels"){
         Canvas c = Canvas(10, 20);
 
         REQUIRE(c.width == Approx(10).margin(EPSILON));
@@ -23,17 +23,18 @@ TEST_CASE("Creating a canvas"){
 }
 
 TEST_CASE("Writing pixels to a canvas"){
-    GIVEN ("A canvas c with width 10 and height 20 pixels, and the color red"){
+    GIVEN("A canvas c with width 10 and height 20 pixels, and the color red"){
         Canvas c = Canvas(10, 20);
         Color red = Color(1, 0, 0);
-
-        c[2][3] = red;
-        REQUIRE(c[2][3] == Color(1, 0, 0));
+        THEN("Write to x = 2 (width), y = 3 (height)"){
+            c[3][2] = red;
+            REQUIRE(c[3][2] == Color(1, 0, 0));
+        }
     }
 }
 
 TEST_CASE("Constructing the PPM header"){
-    GIVEN ("A canvas c with width 5 and height 3 pixels"){
+    GIVEN("A canvas c with width 5 and height 3 pixels"){
         Canvas c = Canvas(5, 3);
         c.to_ppm("test.ppm");
         std::ifstream file(OUTPUT_DIR + "test.ppm");
@@ -49,5 +50,83 @@ TEST_CASE("Constructing the PPM header"){
                 break;
         }
         REQUIRE(header == "P3\n5 3\n255\n");
+    }
+}
+
+TEST_CASE("Constructing the PPM pixel data"){
+    GIVEN("A canvas c with width 5 and height 3 pixels, and three colors"){
+        Canvas c = Canvas(5, 3);
+        Color c1 = Color(1.5, 0, 0);
+        Color c2 = Color(0, 0.5, 0);
+        Color c3 = Color(-0.5, 0, 1);
+
+        c[0][0] = c1;
+        c[1][2] = c2;
+        c[2][4] = c3;
+
+        c.to_ppm("test2.ppm");
+        std::ifstream file(OUTPUT_DIR + "test2.ppm");
+
+        string line;
+        string pixel_data;
+        int line_counter = 0;
+        while(std::getline(file, line)){
+            ++line_counter;
+            if (line_counter >= 4)
+            {
+                if (line_counter >= 7)
+                    break;
+                pixel_data += line.c_str();
+                pixel_data += "\n";
+            }
+        }
+        REQUIRE(pixel_data == "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n");
+    }
+}
+
+TEST_CASE("Splitting long lines in PPM files"){
+    GIVEN("A canvas c with width 10 and height 2 pixels"){
+        Canvas c = Canvas(10, 2);
+        WHEN("Every pixel of c is set to Color(1, 0.8, 0.6) and PPM file is generated"){
+            for (size_t i = 0; i < c.height; ++i){
+                for (size_t j = 0; j < c.width; ++j){
+                    c[i][j] = Color(1, 0.8, 0.6);
+                }
+            }
+            c.to_ppm("test3.ppm");
+        }
+        std::ifstream file(OUTPUT_DIR + "test3.ppm");
+
+        string line;
+        string pixel_data;
+        int line_counter = 0;
+        while(std::getline(file, line)){
+            ++line_counter;
+            if (line_counter >= 4)
+            {
+                if (line_counter >= 8)
+                    break;
+                pixel_data += line.c_str();
+                pixel_data += "\n";
+            }
+        }
+        REQUIRE(pixel_data == "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153\n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153\n");
+    }
+}
+
+TEST_CASE("PPM files are terminated by a new line"){
+    GIVEN("A canvas c with width 5 and height 3 pixels"){
+        Canvas c = Canvas(5, 3);
+        WHEN("PPM is generated"){
+            c.to_ppm("test4.ppm");
+        }
+        std::ifstream file(OUTPUT_DIR + "test4.ppm");
+
+        string line;
+        string last_line;
+        while(std::getline(file, line)){
+        }
+        last_line = line + "\n";
+        REQUIRE(last_line == "\n");
     }
 }
