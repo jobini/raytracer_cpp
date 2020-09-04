b1761c13126c3b9e7ece159cee4829c5cbbb880f
jobini <jobsism10@gmail.com>
2020-09-04 19:14:23 +0530

    unit test for 2x2 matrices

--- test/matrix_tests.cpp
+++ test/matrix_tests.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE("Constructing and inspecting a 4x4 matrix"){
     GIVEN("A 4x4 matrix M"){
-        Matrix M({{1, 2, 3 ,4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12}, {13.5, 14.5, 15.5, 16.5}});
+        Matrix M = {{1, 2, 3 ,4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12}, {13.5, 14.5, 15.5, 16.5}};
 
         REQUIRE(M[0][0] == Approx(1).margin(EPSILON));
         REQUIRE(M[0][3] == Approx(4).margin(EPSILON));
@@ -16,3 +16,15 @@ TEST_CASE("Constructing and inspecting a 4x4 matrix"){
         REQUIRE(M[3][2] == Approx(15.5).margin(EPSILON));
     }
 }
+
+TEST_CASE("A 2x2 matrix ought to be representable"){
+    GIVEN("A 2x2 matrix M"){
+        Matrix M = {{-3, 5}, {1, -2}};
+
+        REQUIRE(M[0][0] == Approx(-3).margin(EPSILON));
+        REQUIRE(M[0][1] == Approx(5).margin(EPSILON));
+        REQUIRE(M[1][0] == Approx(1).margin(EPSILON));
+        REQUIRE(M[1][1] == Approx(-2).margin(EPSILON));
+    }
+}
+
